674bad177e97c5d0b5fbb825ece214dcbf354664
kenken <kenken.gmail.com>
2020-12-28 17:32:50 +0900

    firebaseインストール

---
+++ instagrm/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  instagrm
+//
+//  Created by 中村憲佑 on 2020/12/28.
+//  Copyright © 2020 kensuke.nakamura. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
