b1919165eff46093f25879043011fdcbf5a52a2a
kenken <kenken.gmail.com>
2020-12-29 10:00:27 +0900

    Lesson 8Chapter 9 終了

---
+++ instagrm/Const.swift
@@ -0,0 +1,14 @@
+//
+//  Const.swift
+//  instagrm
+//
+//  Created by 中村憲佑 on 2020/12/29.
+//  Copyright © 2020 kensuke.nakamura. All rights reserved.
+//
+
+import Foundation
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
